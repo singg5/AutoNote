a2f5de040858cf7733e99bf9dab4d9ab82917100
noriban <norbertrom01@gmail.com>
2022-11-28 14:30:19 +0100

    Initial Commit

---
+++ app/src/main/java/com/averis/autonote/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.averis.autonote
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var num1: EditText
+    lateinit var num2: EditText
+    lateinit var num3: EditText
+//    lateinit var num1Str: String
+//    lateinit var num2Str: String
+//    lateinit var num3Str: String
+    lateinit var wynik: TextView
+    lateinit var oblicz: Button
+
+    fun roundToNum(numInDouble: Double): String {
+        return "%.2f".format(numInDouble)
+    }
+
+    fun sum(a: Double, b: Double, c:Double): Double{
+        return a+b+c
+    }
+    fun paliwo(zp:Double, km:Double): Double {
+        return (zp/km)*100
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        num1 = findViewById(R.id.num1)
+        num2 = findViewById(R.id.num2)
+        num3 = findViewById(R.id.num3)
+        oblicz = findViewById(R.id.obliczButt)
+        wynik = findViewById(R.id.wynik)
+
+
+        // przy kliknięciu oblicza paliwo, jednak gdy nie ma danych nic nadaje że jest błąd w
+        // konsoli
+        oblicz.setOnClickListener {
+            var lp = num1.text.toString()
+            var ld = num2.text.toString()
+            var lt = num3.text.toString()
+            if (lp.isEmpty()) {
+                println("error")
+            } else if (ld.isEmpty()){
+                println("error")
+            }
+//            else if (lt.isEmpty()) {
+//                println("error")
+//            }
+            else {
+                var wynikpaliwa: Double = paliwo(ld.toDouble(), lp.toDouble())
+                wynik.setText("${roundToNum(wynikpaliwa)} l/100km")
+            }
+
+        }
+    }
+}
+
+
